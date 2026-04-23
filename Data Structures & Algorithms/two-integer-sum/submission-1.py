2b57206d262bf9d82334fc5501c6e18441e64114
Sheina <47746586+sheinaedeline@users.noreply.github.com>
2026-04-23 16:57:49 +1000

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Dict to store remainder number to sum up into the target
+        remainder_dict = dict()
+
+        # If remainder exists in dict, stop loop
+        for i in range(len(nums)):
+            remainder = target - nums[i]
+            # Found pair
+            if remainder not in remainder_dict:
+                remainder_dict[nums[i]] = i
+            else :
+                return sorted([i, remainder_dict[remainder]])
+            
+        
+            
